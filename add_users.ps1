4afa2e6c84a6f4df1e081c077e40b30db88d67b7
gdeheul <gertdeheul@gmail.com>
2022-03-08 09:10:49 +0100

    Delete add_users.ps1

--- add_users.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module Tridion-CoreService
-Set-TridionCoreServiceSettings -HostName "[HOST_NAME]" -Version Web-8.5 -ConnectionType Default -Persist
-
-New-TridionUser -UserName "[DOMAIN\UserName]"  -Description "Test User 1" -MemberOf "System Administrator"
-
-# For Web 8.5 the -MakeAdministrator option does not work, you have to add the user to the Defensie Intranet Functioneelbeheerders group instead
-# New-TridionUser -UserName "domain\username" -Description "Person Name" -MemberOf "System Administrator"
-
-
